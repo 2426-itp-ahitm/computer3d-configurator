63adaee9565526b5a3870e04565051e628f0f826
JulianMurach <j.murach@students.htl-leonding.ac.at>
2024-11-08 13:19:27 +0100

    #38 set up quarkus

--- backend/src/main/resources/db/import.sql
+++ backend/src/main/resources/db/import.sql
@@ -5,5 +5,6 @@ VALUES
     (false, 2, 0, 'My 2nd ToDo'),
     (false, 3, 0, 'My 3rd ToDo'),
     (false, 4, 0, 'My 4th ToDo'),
-    (false, 5, 0, 'My 5th ToDo')
+    (false, 5, 0, 'My 5th ToDo'),
+    (false, 6, 0, 'My 6th ToDo')
 ;
