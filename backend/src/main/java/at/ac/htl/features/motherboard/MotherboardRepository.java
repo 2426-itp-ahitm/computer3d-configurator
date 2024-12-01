e1e3f4a86aa63bfdf51a54a51b83c46e12db68be
JulianMurach <j.murach@students.htl-leonding.ac.at>
2024-12-01 17:34:58 +0100

    added motherboard check if cpu selected first

--- backend/src/main/java/at/ac/htl/features/motherboard/MotherboardRepository.java
+++ backend/src/main/java/at/ac/htl/features/motherboard/MotherboardRepository.java
@@ -1,8 +1,15 @@
 package at.ac.htl.features.motherboard;
 
+import java.util.List;
+
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class MotherboardRepository implements PanacheRepository<Motherboard> {
+
+    public List<Motherboard> findBySocket(String socket) {
+        return list("socket", socket); // filter by sockets
+    }
+
 }
