01365583db8fa138b5db387abfa022ea3ef1a1e6
RaphaelReisinger <r.reisinger1@students.htl-leonding.ac.at>
2024-12-06 13:42:14 +0100

    #56 frontend

--- frontend/src/cpu/cpu-component.ts
+++ frontend/src/cpu/cpu-component.ts
@@ -2,9 +2,6 @@ import { html, render } from "lit-html"
 import { loadAllCPUs } from "./cpu-service"
 import { CPU } from "src/model"
 
-const styles = html`
-    <link rel="stylesheet" href="../../CSS/style.ccs">
-`
 class CpuComponent extends HTMLElement {
 
     private cpus: CPU[] = []; // Typisieren und initialisieren
@@ -14,6 +11,15 @@ class CpuComponent extends HTMLElement {
         this.attachShadow({ mode: "open" })
     }
     async connectedCallback() {
+
+    const cssResponse = await fetch("../../style.css")
+    const css = await cssResponse.text()
+
+      const styleElement = document.createElement("style")
+      styleElement.textContent = css
+
+      this.shadowRoot.appendChild(styleElement)
+
         this.cpus = await loadAllCPUs()
         this.renderCPUs();
     }
@@ -50,61 +56,7 @@ class CpuComponent extends HTMLElement {
         )
         return html`
                 ${data}
-        <style>
-           .CpuContainer {
-            background-color: rgba(255, 255, 255, 0.409);
-            color: white;
-            padding: 1vw;
-            margin: 1vw;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
-    
-        .CpuDetails {
-            color: white;
-            display: flex;
-            flex-direction: column;
-        }
-    
-        .CpuName {
-            color: white;
-            font-size: 1.5vw;
-            margin-bottom: 1vw;
-        }
-    
-        .ContentWrapper {
-            display: flex;
-            align-items: center;
-        }
-    
-        .Image img {
-            mix-blend-mode: multiply;
-            width: 10vw;
-            height: auto;
-            margin-right: 1.5vw;
-        }
-    
-        .Info {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-        }
-    
-        .addButton {
-            background-color: black;
-            color: white;
-            border: none;
-            padding: 0.8vw 1.5vw;
-            font-size: 1vw;
-            cursor: pointer;
-            border-radius: 1vw;
-            margin-top: 1vw;
-        }
-    
-        .addButton:hover {
-            background-color: #444;
-        }
-        </style>
+                
     `
     }
 
