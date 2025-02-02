2b4d5bd898c017a37bc3b1e8924fd248804b72f0
RaphaelReisinger <r.reisinger1@students.htl-leonding.ac.at>
2025-02-02 12:41:30 +0100

    ordner gelöscht ausserhalb von /feature

--- frontend/src/cpu/cpu-service.ts
+++
@@ -1,9 +0,0 @@
-// import { CPU } from "src/model"
-
-// const BASE_URL = "/api"
-
-// export async function loadAllCPUs() {
-//     const response = await fetch(`${BASE_URL}/cpus`)
-//     const cpus: CPU[] = await response.json()
-//     return cpus
-// }
