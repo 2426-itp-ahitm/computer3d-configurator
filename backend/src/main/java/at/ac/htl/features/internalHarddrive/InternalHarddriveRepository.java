3d1ffa2efcfd2f78a2ccfdf918cf49f91072efe6
Abdulkerim06 <a.cufurovic@students.htl-leonding.ac.at>
2025-03-14 13:09:49 +0100

    Added Application Scoped in InternalHarddriveRepo #60

--- backend/src/main/java/at/ac/htl/features/internalHarddrive/InternalHarddriveRepository.java
+++ backend/src/main/java/at/ac/htl/features/internalHarddrive/InternalHarddriveRepository.java
@@ -1,6 +1,8 @@
 package at.ac.htl.features.internalHarddrive;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class InternalHarddriveRepository implements PanacheRepository<InternalHarddrive> {
 }
