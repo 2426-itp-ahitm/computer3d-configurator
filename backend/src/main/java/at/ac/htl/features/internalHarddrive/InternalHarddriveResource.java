24899816781ae261f51eb2a5c1b31c5b70db4432
MarkusTran <m.tran1@students.htl-leonding.ac.at>
2025-03-23 19:58:30 +0100

    test

--- backend/src/main/java/at/ac/htl/features/internalHarddrive/InternalHarddriveResource.java
+++ backend/src/main/java/at/ac/htl/features/internalHarddrive/InternalHarddriveResource.java
@@ -12,7 +12,7 @@ import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
 
-@Path("internal-harddrive")
+@Path("internalHardDrive")
 @Produces(MediaType.APPLICATION_JSON)
 public class InternalHarddriveResource{
     @Inject InternalHarddriveMapper internalHarddriveMapper;
